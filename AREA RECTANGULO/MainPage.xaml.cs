f51fa326aba9902861c9c509a31b95d23956b281
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-28 08:41:37 -0600

    Agregar archivos de proyecto.

---
+++ AREA RECTANGULO/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AREA_RECTANGULO
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Cal_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double b = Convert.ToDouble(@base.Text);
+                double h = Convert.ToDouble(altura.Text);
+
+                sup.Text = "SUPERFICIE: " + (b * h);
+                per.Text = "PERIMETRO: " + (b + b + h + h);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERROR", "OCURRIO UN ERROR: " + ex.Message, "ACEPTAR");
+            }
+        }
+    }
+}
